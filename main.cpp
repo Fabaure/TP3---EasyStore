3d1476533cabefef4c4cfbb5b55f8c13261539f3
Aurelien Litra <al464089@ad.u-bourgogne.fr>
2024-12-09 16:09:17 +0100

    Edit main.cpp

--- main.cpp
+++ main.cpp
@@ -9,5 +9,9 @@ int main(){
     EasyStore.Ajouterproduct(Product("Lait", "Lait de vache", 14, 2.65));
     EasyStore.Ajouterproduct(Product("Chocolat", "Chocolat au cacao", 27, 8.99));
     EasyStore.AfficherProductALL();
+    EasyStore.AfficherProductNom("Chocolat");
+    EasyStore.AfficherProductNom("Eau");
+    EasyStore.MettreaJourqtite("Eau", 50);
+    EasyStore.AfficherProductNom("Eau");
     return 0;
 }
