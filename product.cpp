bc39ec0aedadddaf5dce4ecf8219c27a4963b6e2
Aurelien Litra <al464089@ad.u-bourgogne.fr>
2024-12-09 14:51:11 +0100

    Class Product

---
+++ product.cpp
@@ -0,0 +1,33 @@
+#include "product.h"
+
+Product::Product(std::string titre, std::string description, int qtite_dispo, float prix)    
+    : titre_(titre), desc_(description),q_dispo_(qtite_dispo),prix_unit_(prix) {}
+
+std::string Product::Get_titre() const
+{
+    return titre_;
+}
+
+std::string Product::Get_description() const
+{
+    return desc_;
+}
+
+int Product::Get_quantite_dispo() const
+{
+    return q_dispo_;
+}
+
+float Product::Get_prix_unitaire() const
+{
+    return prix_unit_;
+}
+
+std::ostream &operator<<(std::ostream &os, const Product &product)
+{
+    os << "Titre du produit : " << product.Get_titre() << "\n"
+       << "Description du produit : " << product.Get_description() << "\n"
+       << "Quantité disponible : " << product.Get_quantite_dispo() << "\n"
+       << "Prix du produit : " << product.Get_prix_unitaire() << "\n";
+    return os;
+}
---
+++ product.h
@@ -0,0 +1,17 @@
+#include <string>
+#include <iostream>
+
+class Product{
+    private:
+        std::string titre_;
+        std::string desc_;
+        int q_dispo_;
+        float prix_unit_;
+    public:
+        Product(std::string titre, std::string description, int qtite_dispo, float prix);
+        std::string Get_titre() const;
+        std::string Get_description() const;
+        int Get_quantite_dispo() const;
+        float Get_prix_unitaire() const;
+        friend std::ostream& operator<<(std::ostream& os, const Product& product);
+};
