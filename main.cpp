2772a1e38468fd185882f577c8a2a84a358c965f
Aurelien Litra <al464089@ad.u-bourgogne.fr>
2024-12-09 14:56:17 +0100

    Main with class magasin

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 
 int main(){
+    Magasin EasyStore;
+    EasyStore.AfficherInfo();
     Product Eau("Eau", "Eau de source", 25000, 1.35);
     std::cout << Eau;
     return 0;
