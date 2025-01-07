d34f10f633d39aaf33a63cc28fbaabf09f12a300
Aurélien LITRA <aurelien.litra@gmail.com>
2025-01-07 11:59:18 +0100

    Ajout test méthode

--- main.cpp
+++ main.cpp
@@ -3,8 +3,10 @@
 
 int main(){
     Magasin EasyStore;
+    Magasin PolyStore;
     EasyStore.AfficherInfo();
     EasyStore.AfficherProductALL();
+    PolyStore.Ajouterproduct(Product("Menthe", "Plante", 5, 1.56));
     EasyStore.Ajouterproduct(Product("Eau", "Eau de source", 25, 1.35));
     EasyStore.Ajouterproduct(Product("Lait", "Lait de vache", 14, 2.65));
     EasyStore.Ajouterproduct(Product("Chocolat", "Chocolat au cacao", 27, 8.99));
@@ -14,7 +16,13 @@ int main(){
     EasyStore.MettreaJourqtite("Eau", 25);
     EasyStore.AfficherProductNom("Eau");
     EasyStore.AfficherInfo();
+
+
+
     Client Aurelien(152, "Aurelien", "LITRA", {});
+    Client Nicolas(153, "Nicolas", "DEFOUR", {});
+
+
     Aurelien.AjouterProduitPanier(EasyStore, "Eau", 5);
     Aurelien.AjouterProduitPanier(EasyStore, "Lait", 1);
     Aurelien.AjouterProduitPanier(EasyStore, "Chocolat", 5);
@@ -30,12 +38,23 @@ int main(){
     EasyStore.AfficherProductNom("Eau");
     EasyStore.AfficherInfo();
     EasyStore.AjouterClient(Aurelien);
+    EasyStore.AfficherInfo();
     EasyStore.AfficherClient();
-    std::cout << "test" << std::endl;
+    std::cout << std::endl;
+    std::cout << "Premier recherche avec le NOM" << std::endl << std::endl;
     EasyStore.AfficherClientNomID("LITRA");
-    std::cout << "test 2" << std::endl;
+    std::cout << std::endl;
+    std::cout << "Deuxieme recherche avec 152" << std::endl << std::endl;
+    EasyStore.AfficherClientNomID("152");
+    std::cout << std::endl;
+    EasyStore.AjouterProductClient(152,"Lait",2);
+    std::cout << std::endl;
     EasyStore.AfficherClientNomID("152");
 
+    std::cout << "test" << std::endl;
+    EasyStore.AjouterProductClient(153,"Lait",2);
+    std::cout << "test2" << std::endl;
+    std::cout << Nicolas;
 
     return 0;
-}
+} 
